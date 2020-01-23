a8a741cf5b6f7a479f62b83589e3056b8411e003
diorellatom <59546574+diorellatom@users.noreply.github.com>
2020-01-23 20:20:47 +0800

    Add files via upload

---
+++ EXAMPLE_DATA/EG_CUSTOMER_INFO_sample.sql
@@ -0,0 +1,6 @@
+INSERT INTO PROTELDBNEW.EG_CUSTOMER_INFO (ID, FIRSTNAME, LASTNAME, DEPARTMENT, JOBTITLE) VALUES
+  (1, 'James', 'Dean', 'Administration', 'Admin Assistant'),
+  (2, 'John', 'Doe', 'Marketing', 'Marketing Manager'),
+  (3, 'Kim', 'Chiu', 'Human Resources', 'HR Representative'),
+  (4, 'Sam', 'Smith', 'IT', 'IT Programmer'),
+  (5, 'Billie', 'Jean', 'Accounting', 'Accountant');
