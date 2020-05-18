966798b9513c48a0eece96540a21775fa14441fc
Okka Pratama <okka.sudaryanto@gmail.com>
2020-05-18 13:51:11 +0700

    Create team_filter.js

---
+++ team-filter-and-direct-assignment/team_filter.js
@@ -0,0 +1,12 @@
+tw.local.filteredTeam = new tw.object.Team();
+
+tw.local.filteredTeam.members = new tw.object.listOf.String();
+
+//filter from full team to only 1 user group
+for(var i = 0; i < tw.local.originalTeam.members.length - 1; i++){
+	tw.local.filteredTeam.members[i] = tw.local.originalTeam.members[i];
+}
+
+
+//filter from full team to only one inputted user
+//tw.local.filteredTeam.members[0] = tw.local.user;
